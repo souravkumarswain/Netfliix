fd4d938444296e4a5b9cb904b9e9281207e30635
souravkumar <souravkumarswain96@gmail.com>
2026-07-03 08:00:28 +0530

    firebase setup for web app. athentication and hosting

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ The React Compiler is not enabled on this template because of its impact on dev 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
 
 
+# For authentication and hosting I am using Firebase as backend. 
+s1- create your project in firebase 
+s2- install firebase tools
+s3-firebase login
+s4- firebase init
+s5-firebase deploy 
+Firebase website has all the informations.
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAFNyZ0A5FVCMhGKnRiIPZbPq90hVj8fQk",
+  authDomain: "netfliix-da9a8.firebaseapp.com",
+  projectId: "netfliix-da9a8",
+  storageBucket: "netfliix-da9a8.firebasestorage.app",
+  messagingSenderId: "503914127190",
+  appId: "1:503914127190:web:11e21612f22512d90b80f7",
+  measurementId: "G-GWQJMNQNRB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
