ee688470cc54fd4f8899894e499080b3b71d3b3e
souravkumar <souravkumarswain96@gmail.com>
2026-07-04 07:56:10 +0530

    Sign up Login setup using firebase logics

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,61 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { validateData } from '../utils/validate'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../utils/firebase';
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true)
   const [errorMsg,setErrorMessage] = useState(null)
+  const [signUpErr, setSignUpErr] = useState(null)
+  const [userName, setUserName] = useState(null)
+  const [userEmail,setUserEmail] = useState(null)
+  const [userPass, setUserPass] = useState(null)
+
   const email = useRef(null)
   const password = useRef(null)
+  const name = useRef(null)
 
   const onToggleSignUp = () => {
     setIsSignIn(!isSignIn);
   }
 
   const onClickButton = () => {
-    const errorResponse = validateData(email.current.value,password.current.value);
-    setErrorMessage(errorResponse);   
+    const errorResponse = validateData(email.current.value, password.current.value);
+    setErrorMessage(errorResponse);
+    if (!isSignIn) {
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if(errorCode || errorMessage){
+            setSignUpErr('Ahhh!! User is already registered')
+          }
+        })
+    }
+    else {
+      //sign in logic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if(errorCode || errorMessage){
+            setErrorMessage('Please try with valid login-id and password!!')
+          }
+        });
+    }
   }
 
   return (
@@ -26,7 +67,7 @@ const Login = () => {
       <form onSubmit={(e) =>e.preventDefault()} className='rounded-lg absolute flex flex-col py-12 px-4 bg-slate-900 text-white opacity-90 top-60 left-0 right-0 w-3/12 mx-auto'>
         <h1 className='text-3xl font-bold py-4'>{isSignIn ? 'Sign In' : 'Sign Up'}</h1>
         {
-          !isSignIn && <input className='p-2 my-2 bg-gray-700 rounded' type='text' placeholder='Enter your name'></input>
+          !isSignIn && <input ref = {name} className='p-2 my-2 bg-gray-700 rounded' type='text' placeholder='Enter your name'></input>
         }
         <input
           ref={email}
@@ -40,7 +81,7 @@ const Login = () => {
           type='password'
           placeholder='Enter your password'>
         </input>
-        <p className='text-red-700 font-bold'>{errorMsg}</p>
+        <p className='text-red-700 font-bold'>{!isSignIn?signUpErr:errorMsg}</p>
         <button onClick={onClickButton} className='cursor-pointer bg-red-700 text-white font-bold my-4 p-3 rounded w-full'>{isSignIn ? 'Sign in' : 'Sign up'}</button>
         <h2 className='my-6'>{isSignIn ? 'New to Netflix?' : 'Already a user?'} <span onClick={onToggleSignUp} className='cursor-pointer hover:font-bold'>{isSignIn ? 'Sign Up' : 'Sign In'}</span> here. </h2>
       </form>
